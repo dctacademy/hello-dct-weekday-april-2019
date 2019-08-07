70cc6a42d6ff7c081225ff0045f8c98366368892
Soumya <soumyagidd96@gmail.com>
2019-08-07 09:53:29 +0530

    display function added

--- sample.js
+++ sample.js
@@ -1,7 +1,8 @@
-function add(a, b){
-    return a + b;
-}
+const arr = [1, 2, 3, 5, 6, 8]
 
-function sub(a, b){
-    return a - b;
+function display(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    }
 }
+display(arr)
